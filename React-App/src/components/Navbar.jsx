bd4bb6d42b8cc260cacc55e5c41ea90831f65c36
Ikkyu <ikkyuli2023@gmail.com>
2025-06-17 01:18:42 -0700

    #10 Used basic CSS format for the pages, including top bar position, text colour, text alignment, list style and text sizes.

--- React-App/src/components/Navbar.jsx
+++ React-App/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
+import "./Navbar.css"
 
 export function Navbar() {
     return (
-        <>
-            <Link to="/"><button>Home</button></Link>
-            <Link to="/SkillsLanguages"><button>Skills&Languages</button></Link>
-            <Link to="/EducationExperience"><button>Education&Experience</button></Link>
-            <Link to="/ProjectsInterests"><button>Projects&Interests</button></Link>
-            <Link to="/BlogContact"><button>Blog&Contact</button></Link>
-        </>
+        <div className="navbar">
+            <Link to="/"><button className="navbutton">Home</button></Link>
+            <Link to="/SkillsLanguages"><button className="navbutton">Skills&Languages</button></Link>
+            <Link to="/EducationExperience"><button className="navbutton">Education&Experience</button></Link>
+            <Link to="/ProjectsInterests"><button className="navbutton">Projects&Interests</button></Link>
+            <Link to="/BlogContact"><button className="navbutton">Blog&Contact</button></Link>
+        </div>
     )
 }
